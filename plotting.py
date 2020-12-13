f8f69ea655f552ecd2ce33888b186ce979a295ba
Pankaj Barnwal <suno.pankaj@gmail.com>
2020-12-13 11:54:54 +0530

    chores/renamed the file

---
+++ plotting.py
@@ -0,0 +1,11 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+from matplotlib import interactive
+
+interactive(True)
+
+x = [1, 2, 3]
+y = [5, 64, 7]
+
+plt.plot(x, y, label='first line')
+plt.show()
